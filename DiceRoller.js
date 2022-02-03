3b0764012ee01d9f62bcb25c559d0f8348d975d5
Shane Frantz <shanesfrantz@gmail.com>
2022-02-02 19:09:12 -0600

    added comments and edited the README file

--- DiceRoller.js
+++ DiceRoller.js
@@ -1,5 +1,5 @@
 function rollDice() {
-
+    //variables for dice numbers
     var die1 = rollNumber();
     var die2 = rollNumber();
     var die3 = rollNumber();
@@ -9,6 +9,8 @@ function rollDice() {
     document.getElementById("diceText").innerHTML = "Dice Values: " + die1 + " " + die2 + " " + die3 + " " + die4 + " " + die5;
 }
 
+//function to return random value between 1-6 (inclusive on both ends)
+//created to avoid repetetive Math.random statements when initializing variables
 function rollNumber() {
     return Math.floor((Math.random() * 6) + 1);
 }
